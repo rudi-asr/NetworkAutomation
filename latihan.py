4e726ad4138705103c7c1d5f89998cd977f5e590
rudi-asr <64144763+rudi-asr@users.noreply.github.com>
2023-09-20 21:52:40 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -1,1 +1,12 @@
+# ini baris komentar satu baris
+print("perintah diatas untuk memberikan keterangan/komentar 1 baris")
+
+print("baris komentar/keterangan dapat dilakukan disamping") # baris komentar satu baris disamping
+
+print("Jika ada 2 baris atau lebih bari komentar dapat dilakukan dengan perintah seperti dibawah ini")
+"""
+baris komentar pertama
+baris komentar kedua
+baris komentar ketiga
+"""
 
